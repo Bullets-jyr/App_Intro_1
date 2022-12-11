1b67af972b8b8b3c31820ce8041bb6049fccf63a
Bullets-jyr <jyr@bullets.co.kr>
2022-12-11 15:49:36 +0900

    앱 개발 사전 지식 1

--- App_Intro_1/AppDelegate.swift
+++ App_Intro_1/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// 앱의 최초실행
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 최초실행 function
+        
         return true
     }
 
